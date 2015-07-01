58d8d883f943f2558e214f3e338c0197ad574074
Josh Endter <jendter@users.noreply.github.com>
2015-07-01 12:35:35 -0400

    Initial Commit

---
+++ RottenMangoes/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RottenMangoes
+//
+//  Created by Josh Endter on 7/1/15.
+//  Copyright (c) 2015 Josh Endter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RottenMangoes/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RottenMangoes
+//
+//  Created by Josh Endter on 7/1/15.
+//  Copyright (c) 2015 Josh Endter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
